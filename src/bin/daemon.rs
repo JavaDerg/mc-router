42f14d94b051b2a8f70cdabeac82f8ed94f14409
post-rex <post-rex@pm.me>
2021-03-01 23:55:14 +0100

    Multi bin

---
+++ src/bin/daemon.rs
@@ -0,0 +1,104 @@
+use futures::StreamExt;
+use mc_router::{cprot, SOCKET_PATH};
+use std::path::Path;
+use tokio::io::AsyncWriteExt;
+use tokio::net::unix::WriteHalf;
+use tokio::net::UnixStream;
+use tokio_util::codec::{FramedRead, LinesCodec};
+
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
+#[tokio::main]
+async fn main() {
+	tracing_subscriber::fmt()
+		.with_max_level(tracing::Level::INFO)
+		.init();
+
+	let socket = Path::new(SOCKET_PATH);
+
+	// Remove old socket
+	let _ = std::fs::remove_file(socket);
+
+	let listener = match tokio::net::UnixListener::bind(socket) {
+		Ok(l) => l,
+		Err(e) => {
+			tracing::error!("Can not create unix socket; Path={}; Reason={}", SOCKET_PATH, e);
+			std::process::exit(1);
+		}
+	};
+
+	loop {
+		let (socket, addr) = match listener.accept().await {
+			Ok(s) => s,
+			Err(e) => {
+				tracing::error!("Unable to accept client; Reason={}", e);
+				continue;
+			}
+		};
+		tracing::info!("New control connection; Addr={:?}", addr);
+		tokio::spawn(handle_controller(socket));
+	}
+}
+
+async fn handle_controller(mut stream: UnixStream) {
+	let (read, mut write) = stream.split();
+	let mut framed = FramedRead::new(read, LinesCodec::new_with_max_length(8192));
+	while let Some(line) = framed.next().await {
+		let line = match line {
+			Ok(line) => line,
+			Err(err) => {
+				tracing::warn!("Failed to read packet; State=Closing connection; Error={}", err);
+				tracing::info!("Closing control connection");
+				return;
+			}
+		};
+		let ron: cprot::Request = match ron::from_str(&line) {
+			Ok(obj) => obj,
+			Err(err) => {
+				tracing::warn!(
+					"Received Invalid Packet; State=Closing connection; Error={}; Data={}",
+					err,
+					line
+				);
+				let _ = write_res(
+					&mut write,
+					cprot::Response::Error(
+						cprot::ErrKind::InvalidPacket,
+						String::from("Unable to decode packet"),
+					),
+				)
+				.await;
+				return;
+			}
+		};
+		tracing::info!("Received Packet; Parsed={:?}", ron);
+		if write_res(&mut write, process_packet(ron).await).await.is_err() {
+			return;
+		}
+	}
+}
+
+async fn process_packet(packet: cprot::Request) -> cprot::Response {
+	match packet {
+		cprot::Request::Echo => cprot::Response::Echo,
+		_ => cprot::Response::Nil,
+	}
+}
+
+async fn write_res(wh: &mut WriteHalf<'_>, res: cprot::Response) -> tokio::io::Result<()> {
+	let out = wh
+		.write_all(
+			format!(
+				"{}\n",
+				ron::to_string(&res)
+					.expect("If you can read this, email <post-rex@pm.me> about hot glue + stacktrace")
+			)
+			.as_bytes(),
+		)
+		.await;
+	if out.is_err() {
+		tracing::error!("Unable to send packet; Error={}", out.as_ref().unwrap_err())
+	}
+	out
+}
